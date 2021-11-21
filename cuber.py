50cb72c8970a18f5c631fb8454be1c9f4af0be78
rvansick <rvansick@sfu.ca>
2021-11-21 14:00:23 -0800

    hi paul allan

--- cuber.py
+++ cuber.py
@@ -1,3 +1,28 @@
 # CMPT 417/427
 # Rubiks Cube solving
+# General cube class
 # Ryan Vansickle
+
+# import numpy as np
+# based on preil research, standard python lists are actually
+# faster than numpy here unless we go to big n
+
+class Cube:
+  n = 0
+  # dimensions of cube
+  path = []
+  # path to solution (if exists, to be passed to next search step instances)
+  
+  # 6 faces of cube, (F)ore (B)ack (L)eft (R)ight (U)p (D)own
+  F = [[]]
+  B = [[]]
+  L = [[]]
+  R = [[]]
+  U = [[]]
+  D = [[]]
+    
+  
+  
+  def __init__(self, n, [F, B, L, R, U, D]):
+    self.n = n
+    
