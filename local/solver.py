b4d57f316b3c4827d2fb758d76db66055ddfd1f4
jla673 <>
2021-12-18 22:41:59 -0800

    optimization and cleanup
    Finished push and pop function.
    Made move run in constant time.
    Added comments for clarity.

--- local/solver.py
+++ local/solver.py
@@ -1,4 +1,5 @@
 from cube import Cube
+import heapq
 import random
 
 def scramble(i,n,cube):
@@ -10,23 +11,19 @@ def scramble(i,n,cube):
         cube.transform(r2,r1,r3)
 
 def heuristic(cube):
-    pass
+    return 1
 def move(n, index):
-    # TODO find a way to optumize this later
-    # theres probably a better way to do this cause this just seems slow 
-    movement = []
-    c = [-1,1]
-    for i in range(1,n+1):
-        for j in range(0,3):
-            for k in c:
-                movement.append([i,j,k])
-    return movement[index]
-def push(node):
-    # TODO make this function
-    pass
-def pop():
-    # TODO make this function
-    pass
+    n_val = index%n
+    c_val = index//n
+    a_val = c_val%3
+    r_val = 1 if c_val//3 == 1 else -1
+    return [n_val+1,a_val,r_val]
+def push(open_list, node):
+    heapq.heappush(open_list, (node['g_val'] + node['h_val'], node['h_val'], node['cube'], node))
+def pop(open_list):
+    _, _, _, curr = heapq.heappop(open_list)
+    return curr
+# basically a depth first search without a proper heuristic
 def IDAStar(cube):
     # starting node
     start = {
@@ -41,63 +38,69 @@ def IDAStar(cube):
     nextBound = heuristic(cube)
     currCube = cube
     while not currCube.solved():
+        # when nodestack is empty it means either it was just initialized or 
+        # we return to the root after a branch doesnt have a solution within the bounds
         if len(nodeStack) == 0:
             bound = nextBound
             nextBound*=2
             nodeStack.append(start)
+        # set the current nodes
         currNode = nodeStack.pop()
         currCube = currNode["cube"]
+        # if it is bound check if the problem has been solved if it has then end 
+        # redundant to add a condition for the positive
+        # cannot be above bound by the nature of how elements were pushed
         if currNode["g_val"] != bound:
+            # make all moves possible
             for i in range(2*3*cube.n):
+                # create the states of the cubes
                 m = move(cube.n,i)
+                # trust that this is a deep copy otherwise it can lead to bugs
                 copy = Cube(currNode["cube"])
                 copy.transform(m[0],m[1],m[2])
                 node = {"cube": copy, "h_val": heuristic(cube), "g_val": \
                     currNode["g_val"] + 1, "transform": m}
                 if(node["h_val"]+node["g_val"]<=bound):
-                    push(node)
+                    push(successors,node)
                 elif(node["h_val"]+node["g_val"]<nextBound):
                     nextBound = node["h_val"]+node["g_val"]
-        # I honestly dont know if this is right but it seems right to me
-        # Theres a chance that the first while loop will cause a bug
-        temp = 0
-        while(len(successors)!=0):
-            temp = []
-            temp.append(pop())
-        while(len(temp)!=0):
-            tnode = temp.pop()
-            node = {"cube": tnode["cube"],"g_val":tnode["g_val"],
-                "transform": tnode["transform"],"parent":currNode}
-            nodeStack.append(node)
+            # I'm not sure if i should reverse this but reversing seems correct to me 
+            # if it causes an error comment out or remove the reverse()
+            successors.reverse()
+            while(len(successors)!=0):
+                tnode = pop(successors)
+                node = {"cube": tnode["cube"],"g_val":tnode["g_val"],
+                    "transform": tnode["transform"],"parent":currNode}
+                nodeStack.append(node)
     return currNode
 
 # get the path 
 def getPath(node):
     currNode = node
     path = []
-    temp = []
     while currNode["transform"]!=None:
-        temp.append(currNode["transform"])
+        path.append(currNode["transform"])
         currNode = currNode["parent"]
-    while len(temp)!=0:
-        path.append(temp.pop())
+    path.reverse()
     return path
 
-        
+# testing purposes
+
+
 # transform(1,1) turns front face
 # transform(1,0) turns right face
 # transform(1,2) turns bottom face
-cube = Cube(None)
-scramble(20,3,cube)
+# cube = Cube(None)
+# scramble(20,3,cube)
 # cube.transform(1,1,-1)
 
-
-
+# for i in range(18):
+#     print(move(3,i))
 
         
 
 
 
-cube.draw3()
+# cube.draw3()
 
 
